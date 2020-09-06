fe960bbef28cdcf4e6387ce08af71959d1ccadb1
sandy1709 <58665444+sandy1709@users.noreply.github.com>
2020-09-06 07:41:57 +0000

    pylint: auto fixes

--- userbot/plugins/secret.py
+++ userbot/plugins/secret.py
@@ -16,7 +16,7 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
             query = query[7:]
             user, txct = query.split(" ", 1)
             try:
-                #if u is user id
+                # if u is user id
                 u = int(user)
                 buttons = [custom.Button.inline(
                     "show message 🔐",
@@ -36,7 +36,7 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
                     buttons=buttons)
                 await event.answer([result] if result else None)
             except ValueError:
-                #if u is username 
+                # if u is username
                 u = await event.client.get_entity(user)
                 buttons = [custom.Button.inline(
                     "show message 🔐",
@@ -50,7 +50,7 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
                     text=f"🔒 A whisper message to {sandy}, Only he/she can open it.",
                     buttons=buttons)
                 await event.answer([result] if result else None)
-                
+
     @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"secret_(.+?)_(.+)")))
     async def on_plug_in_callback_query_handler(event):
         userid = event.pattern_match.group(1)
@@ -63,4 +63,4 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
             await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
         else:
             reply_pop_up_alert = "why were you looking at this shit go away and do your own work,idiot"
-            await event.answer(reply_pop_up_alert, cache_time=0, alert=True)                
+            await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
